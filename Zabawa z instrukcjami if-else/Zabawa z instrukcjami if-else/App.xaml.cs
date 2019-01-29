5a9fc229b6617c62fa5204495d4a70f0d0013365
GrzechuZiomal <35500540+GrzechuZiomal@users.noreply.github.com>
2019-01-29 15:22:03 +0100

    Zabawa z instrukcjami if-else

---
+++ Zabawa z instrukcjami if-else/Zabawa z instrukcjami if-else/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Zabawa_z_instrukcjami_if_else
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Zabawa z instrukcjami if-else/Zabawa z instrukcjami if-else/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zabawa_z_instrukcjami_if_else
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void changeText_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (enableCheckbox.IsChecked == true)
+            {
+                if (labelToChange.Text == "Z prawej")
+                {
+                    labelToChange.Text = "Z lewej";
+                    labelToChange.HorizontalAlignment = HorizontalAlignment.Left;
+                }
+                else
+                {
+                    labelToChange.Text = "Z prawej";
+                    labelToChange.HorizontalAlignment = HorizontalAlignment.Right;
+                }
+            }
+            else
+            {
+                labelToChange.Text = "Mozliwość zmiany tekstu została wyłączona ";
+                labelToChange.HorizontalAlignment = HorizontalAlignment.Center;
+            }
+        }
+    }
+}
